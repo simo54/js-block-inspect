17cf87f87fc85333a09d840c8023ad1d8e68c67f
scava <sorzesimone1@gmail.com>
2022-08-27 10:32:46 +0200

    update module

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
 function CombinationListener(e) {
-	var evtobj = window.event ? event : e;
+	const evtobj = window.event ? event : e;
 	if (evtobj.keyCode == 73 && evtobj.ctrlKey && evtobj.shiftKey);
 	e.preventDefault();
 	return;
 }
 
-document.onkeydown = CombinationListener;
-document.addEventListener('contextmenu', event => event.preventDefault());
+function blockInspectTab() {
+	document.onkeydown = CombinationListener;
+	document.addEventListener('contextmenu', (event) => event.preventDefault());
+}
+
+module.exports = blockInspectTab;
